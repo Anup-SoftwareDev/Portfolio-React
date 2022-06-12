601b035ac49b19a9b16d7b682b346938cab8b712
Anup-SoftwareDev <joseph.anupsa@gmail.com>
2022-06-12 10:21:53 +1000

    formspree functionion as required

--- src/components/Contact/Form.js
+++ src/components/Contact/Form.js
@@ -1,39 +1,66 @@
 import React from 'react';
 import { useForm, ValidationError } from '@formspree/react';
+import classes from './Contact.module.css';
 
 function ContactForm() {
   const [state, handleSubmit] = useForm("xrgjdalo");
-  if (state.succeeded) {
-      return <p>Thanks for joining!</p>;
-  }
+ // if (state.succeeded) {
+   //   return <p>Thanks for joining!</p>;
+  //}
   return (
+    <div className = {classes.MyForm} >
       <form onSubmit={handleSubmit}>
-      <label htmlFor="email">
-        Email Address
-      </label>
-      <input
-        id="email"
-        type="email" 
-        name="email"
-      />
-      <ValidationError 
-        prefix="Email" 
-        field="email"
-        errors={state.errors}
-      />
-      <textarea
-        id="message"
-        name="message"
-      />
-      <ValidationError 
-        prefix="Message" 
-        field="message"
-        errors={state.errors}
-      />
-      <button type="submit" disabled={state.submitting}>
-        Submit
-      </button>
+      <div>
+        <label htmlFor="email">
+          Email Address
+        </label>
+        {state.succeeded?
+        <input
+          id=""
+          type="" 
+          name=""
+          value = ""
+        />:
+        <input
+          id="email"
+          type="email" 
+          name="email"
+        />
+        }
+        <ValidationError 
+          prefix="Email" 
+          field="email"
+          errors={state.errors}
+        />
+       </div> 
+
+       <div> 
+            <label htmlFor="email">
+              Message
+            </label>
+            {state.succeeded?
+            <textarea
+              id=""
+              name=""
+              value=""
+            />:
+            <textarea
+            id="message"
+            name="message"
+            />}
+            <ValidationError 
+              prefix="Message" 
+              field="message"
+              errors={state.errors}
+            />
+       </div> 
+        <button type="submit" disabled={state.submitting}>
+          Submit
+        </button>
+      {state.succeeded&&
+      <p>Thank you for the message. I will get back to you shortly!</p>}
     </form>
+    </div>
   );
 }
 
