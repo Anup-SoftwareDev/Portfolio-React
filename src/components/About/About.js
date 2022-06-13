2f0d7f4f0bd7ef86ed079600bc426789e25a4904
Anup-SoftwareDev <joseph.anupsa@gmail.com>
2022-06-14 00:46:24 +1000

    Resume and Project button in About Me section

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,8 +1,8 @@
 import classes from './About.module.css';
 import PageHeader from './../PageHeader/PageHeader'
 import Anup from './../../img/Anup.png';
-
-
+import resume from './Resume.pdf';
+import React from 'react';
 
 const About = () => {
     return ( 
@@ -10,11 +10,14 @@ const About = () => {
             <PageHeader title={'About Me'}/>
             <div className = {classes.Container}>
                 <div className = {classes.Text}>
-                    <h2>Hello! I'm Anup</h2>
+                    <h2>Hello! I'm Anup 🙂</h2>
                     <p>
                     I am a Full Stack Web
 Developer with a background in Electronic Engineering.I am passionate about making lives of people around me better through Software Development. When I am not developing software, I love spending time outdoors and playing various sports. 
                     </p>
+                    <a href={resume} target="_blank" rel="noopener noreferrer"> <button>View My Resume</button></a>
+                    <a href='./#skills'> <button>View My Projects</button></a>
+
                 </div>
                 <div className = {classes.Photo}>
                     <img className = {classes.Me} src={Anup} alt = "Anup"></img>
