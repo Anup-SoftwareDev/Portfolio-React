06f80f5da76688733a31cf16cf1a151a91f037f6
Anup-SoftwareDev <joseph.anupsa@gmail.com>
2022-08-30 11:55:57 +1000

    About folder rebuilt

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,7 +1,8 @@
 import classes from './About.module.css';
 import PageHeader from './../PageHeader/PageHeader'
-import Anup from './../../img/Anup.jpeg';
-
+import Anup from './../../img/Anup.png';
+import resume from './Resume.pdf';
+import React from 'react';
 
 const About = () => {
     return ( 
@@ -9,14 +10,19 @@ const About = () => {
             <PageHeader title={'About Me'}/>
             <div className = {classes.Container}>
                 <div className = {classes.Text}>
-                    <h2>Hello! I'm Anup</h2>
+                    <h2>Hello! I'm Anup 🙂</h2>
                     <p>
-                    I am passionate about making lives of people around me better through Software Development. When I am not developing software, I love spending time outdoors and playing various sports. I specialise in Web Development for small and large organisations. I have a degree in Electronic engineering and hence also have exposure to Embedded systems programming.
+                    I am a Full Stack Web
+Developer with a background in Electronic Engineering.I am passionate about making lives of people around me better through Software Development. When I am not developing software, I love spending time outdoors and playing various sports. 
                     </p>
+                    <a href={resume} target="_blank" rel="noopener noreferrer"> <button>View My Resume</button></a>
+                    <a href='./#skills'> <button>View My Projects</button></a>
+
                 </div>
                 <div className = {classes.Photo}>
                     <img className = {classes.Me} src={Anup} alt = "Anup"></img>
                 </div>
+
             </div>
         </div>
      );
