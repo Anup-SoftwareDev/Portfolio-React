a99b99a1d5581b4851f441f2fb4183f9e3231697
Anup-SoftwareDev <joseph.anupsa@gmail.com>
2023-09-08 12:31:38 +1000

    added links and icons to Appstore and Testlight

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -6,6 +6,8 @@ import NorthernDetailing from './../../img/NorthernDetailing.png';
 import MyShop from './../../img/MyShop.png';
 import { FaGithub} from "react-icons/fa";
 import {BsGlobe} from "react-icons/bs";
+import Testflight from './../../img/Testflight.png';
+import Appstore  from './../../img/Appstore.png';
 
 
 import projects from './projects.json';
@@ -54,13 +56,23 @@ const Skills = () => {
                                             {(project.url)&&
                                                 <div className = {classes.Url}>
                                                 <BsGlobe color = 'white' size='30px' style ={{padding: '0%'}} onClick={handleURL(project.url)}/>
-                                                <a href={project.url} target="_blank" rel="noopener noreferrer">{}</a>
+                                                <a href={project.url} target="_blank" rel="noopener noreferrer">Website Link</a>
                                             </div>}
                                             {(project.github)&&
                                             <div className = {classes.Github}>
                                                 <FaGithub color = 'white' size='30px' style ={{padding: '0%'}} onClick={handleURL(project.github)}/>
-                                                <a href={project.github} target="_blank" rel="noopener noreferrer">{}</a>
-                                            </div>}          
+                                                <a href={project.github} target="_blank" rel="noopener noreferrer">GitHub Link</a>
+                                            </div>}  
+                                            {(project.testflight)&&
+                                            <div className = {classes.Testflight}>
+                                                <img src={Testflight} alt="Testflight" onClick={handleURL(project.testflight)} style = {{padding: '0%', height: '30px' }} />
+                                                <a href={project.testflight} target="_blank" rel="noopener noreferrer">Testflight Link</a>
+                                            </div>}
+                                            {(project.appstore)&&
+                                            <div className = {classes.Appstore}>
+                                                <img src={Appstore} alt="Appstore" onClick={handleURL(project.appstore)} style = {{padding: '0%', height: '30px' }} />
+                                                <a href={project.appstore} target="_blank" rel="noopener noreferrer">Appstore Link</a>
+                                            </div>} 
                             </div>   
                                     
                                 </div>
